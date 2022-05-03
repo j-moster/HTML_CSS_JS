9dfc7809987539665ee3a17ec4a67da71f31399e
monster <343418638@qq.com>
2022-05-03 18:03:00 +0800

    第五次提交

---
+++ 移动端开发/京东使用swiper插件实现轮播图/京东/js/index.js
@@ -0,0 +1,19 @@
+window.addEventListener('load', function() {
+    var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+            delay: 2000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+
+})
